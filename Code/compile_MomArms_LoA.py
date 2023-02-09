73aa003aa7c41db4fb0a16f73f234f10120ac52d
Aaron Fox <aaron.f@deakin.edu.au>
2023-02-09 15:20:15 +1100

    Bulk data upload.

--- Code/compile_MomArms_LoA.py
+++ Code/compile_MomArms_LoA.py
@@ -25,9 +25,177 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
 # import math
-# from skimage import io
+from skimage import io
 # import re
 # from shutil import copy2, rmtree
+
+# %% Define functions
+
+#Circle fitting function
+def fitCircle(xPoints, yPoints):
+    
+    #Inputs:
+        #xPoints - numpy array of x coordinates
+        #yPoints - numpy array of y coordinates
+    
+    #Calculate means of XY points
+    x_m = np.mean(xPoints)
+    y_m = np.mean(yPoints)
+    
+    #Calculation of the reduced coordinates
+    u = xPoints - x_m
+    v = yPoints - y_m
+    
+    #Linear system defining the center (uc, vc) in reduced coordinates:
+    Suv  = sum(u*v)
+    Suu  = sum(u**2)
+    Svv  = sum(v**2)
+    Suuv = sum(u**2 * v)
+    Suvv = sum(u * v**2)
+    Suuu = sum(u**3)
+    Svvv = sum(v**3)
+    
+    #Solving the linear system
+    A = np.array([ [ Suu, Suv ], [Suv, Svv]])
+    B = np.array([ Suuu + Suvv, Svvv + Suuv ])/2.0
+    uc, vc = np.linalg.solve(A, B)
+    
+    #Get centres
+    centreX = x_m + uc
+    centreY = y_m + vc
+    
+    #Calculate distance to centre and radius
+    Ri = np.sqrt((xPoints-centreX)**2 + (yPoints-centreY)**2)
+    radius = np.mean(Ri)
+    
+    return centreX, centreY, radius
+
+#Identifying 2D line intersections
+def lineIntersect(Ax1, Ay1, Ax2, Ay2, Bx1, By1, Bx2, By2):
+    #returns a (x, y) tuple or None if there is no intersection """
+    d = (By2 - By1) * (Ax2 - Ax1) - (Bx2 - Bx1) * (Ay2 - Ay1)
+    uA = ((Bx2 - Bx1) * (Ay1 - By1) - (By2 - By1) * (Ax1 - Bx1)) / d
+    ###uB = ((Ax2 - Ax1) * (Ay1 - By1) - (Ay2 - Ay1) * (Ax1 - Bx1)) / d
+    x = Ax1 + uA * (Ax2 - Ax1)
+    y = Ay1 + uA * (Ay2 - Ay1)
+ 
+    return x, y
+
+#Procrustes function for identifying transform
+def procrustes(X, Y, scaling = False, reflection = 'best'):
+    
+    """
+    A port of MATLAB's `procrustes` function to Numpy.
+    
+    See: https://stackoverflow.com/questions/18925181/procrustes-analysis-with-numpy
+
+    Procrustes analysis determines a linear transformation (translation,
+    reflection, orthogonal rotation and scaling) of the points in Y to best
+    conform them to the points in matrix X, using the sum of squared errors
+    as the goodness of fit criterion.
+
+        d, Z, [tform] = procrustes(X, Y)
+
+    Inputs:
+    ------------
+    X, Y    
+        matrices of target and input coordinates. they must have equal
+        numbers of  points (rows), but Y may have fewer dimensions
+        (columns) than X.
+
+    scaling 
+        if False, the scaling component of the transformation is forced
+        to 1
+
+    reflection
+        if 'best' (default), the transformation solution may or may not
+        include a reflection component, depending on which fits the data
+        best. setting reflection to True or False forces a solution with
+        reflection or no reflection respectively.
+
+    Outputs
+    ------------
+    d       
+        the residual sum of squared errors, normalized according to a
+        measure of the scale of X, ((X - X.mean(0))**2).sum()
+
+    Z
+        the matrix of transformed Y-values
+
+    tform   
+        a dict specifying the rotation, translation and scaling that
+        maps X --> Y
+
+    """
+
+    n,m = X.shape
+    ny,my = Y.shape
+
+    muX = X.mean(0)
+    muY = Y.mean(0)
+
+    X0 = X - muX
+    Y0 = Y - muY
+
+    ssX = (X0**2.).sum()
+    ssY = (Y0**2.).sum()
+
+    # centred Frobenius norm
+    normX = np.sqrt(ssX)
+    normY = np.sqrt(ssY)
+
+    # scale to equal (unit) norm
+    X0 /= normX
+    Y0 /= normY
+
+    if my < m:
+        Y0 = np.concatenate((Y0, np.zeros(n, m-my)),0)
+
+    # optimum rotation matrix of Y
+    A = np.dot(X0.T, Y0)
+    U,s,Vt = np.linalg.svd(A,full_matrices=False)
+    V = Vt.T
+    T = np.dot(V, U.T)
+
+    if reflection != 'best':
+
+        # does the current solution use a reflection?
+        have_reflection = np.linalg.det(T) < 0
+
+        # if that's not what was specified, force another reflection
+        if reflection != have_reflection:
+            V[:,-1] *= -1
+            s[-1] *= -1
+            T = np.dot(V, U.T)
+
+    traceTA = s.sum()
+
+    if scaling:
+
+        # optimum scaling of Y
+        b = traceTA * normX / normY
+
+        # standarised distance between X and b*Y*T + c
+        d = 1 - traceTA**2
+
+        # transformed coords
+        Z = normX*traceTA*np.dot(Y0, T) + muX
+
+    else:
+        b = 1
+        d = 1 + ssY/ssX - 2 * traceTA * normY / normX
+        Z = normY*np.dot(Y0, T) + muX
+
+    # transformation matrix
+    if my < m:
+        T = T[:my,:]
+    c = muX - b*np.dot(muY, T)
+    
+    #transformation values 
+    tform = {'rotation':T, 'scale':b, 'translation':c}
+   
+    return d, Z, tform
+
 
 # %% Parameters to change
 
@@ -663,6 +831,203 @@ plt.tight_layout()
 
 #Save figure
 fig.savefig('..\\Results\\Figures\\ma_Fig.png', dpi = 300, format = 'png')
+
+#Close figure
+plt.close()
+
+# %% Visualise methodological processes on radiograph
+
+#Uses S4 90 abduction, 40N, scapular plane image, ss4
+
+#Create figure
+fig, ax = plt.subplots(figsize = (20,6), nrows = 1, ncols = 4)
+
+#Load image
+img = io.imread('S4_l\\split50\\abd90_40N_SP_D0302_T1333\\abd90_40N_SP_D0302_T1333_1_proc.tif')
+
+#Turn off axes and display image in each
+for currAx in ax.flatten():
+    #Turn off axes
+    currAx.axis('off')
+    #Display image
+    currAx.imshow(img, cmap = 'gray', origin = 'upper', vmin = 0, vmax = 255)
+    
+#Set tight layout
+plt.tight_layout()
+
+#Get the digitised points data for this reference image
+#Subscap points
+ssPts = pd.read_csv('S4_l\\split50\\abd90_40N_SP_D0302_T1333\\ss4.csv')
+ssPts_x = ssPts['X'].to_numpy()
+ssPts_y = ssPts['Y'].to_numpy()
+#Humeral head points
+hhPts = pd.read_csv('S4_l\\split50\\abd90_40N_SP_D0302_T1333\\hh.csv')
+hhPts_x = hhPts['X'].to_numpy()
+hhPts_y = hhPts['Y'].to_numpy()
+
+#Glenoid plane points
+#Need to calculate via reference points
+#Estimate glenoid plane based on image transformation
+
+#Import the digitised reference points from the current image
+transPts = pd.read_csv('S4_l\\split50\\abd90_40N_SP_D0302_T1333\\pts.csv')
+transPts = np.transpose(np.array([transPts['X'].to_numpy(), transPts['Y'].to_numpy()]))
+
+#Get original digitised points and glenoid pts
+refPts = pd.read_csv('S4_l\\split50\\abd0_0N_SP_D0302_T1308\\pts.csv')
+refPts = np.transpose(np.array([refPts['X'].to_numpy(), refPts['Y'].to_numpy()]))
+
+#Calculate procrustes transformation between points
+resErr, newPts, tform = procrustes(refPts, transPts)
+
+#Get the original gp points
+gpPts = pd.read_csv('S4_l\\split50\\abd0_0N_SP_D0302_T1308\\gp.csv')
+gpPts = np.transpose(np.array([gpPts['X'].to_numpy(), gpPts['Y'].to_numpy()]))
+
+#Apply the transformation to the reference glenoid plane points
+#Transform reference glenoid points to new array
+gpPts_trans = np.zeros(gpPts.shape)
+for pp in range(gpPts.shape[0]):
+    gpPts_trans[pp] = tform['rotation'].dot(gpPts[pp]) - tform['translation']
+    
+#Convert to same format
+gpPts_x = gpPts_trans[:,0]
+gpPts_y = gpPts_trans[:,1]
+ 
+#Display digitised points on first image
+#Subscap points
+ax.flatten()[0].scatter(ssPts_x, ssPts_y, s = 20, c = 'red', zorder = 5)
+#Glenoid plane points
+ax.flatten()[0].scatter(gpPts_x, gpPts_y, s = 20, c = 'yellow', zorder = 4)
+#Humeral head points
+ax.flatten()[0].scatter(hhPts_x, hhPts_y, s = 20, c = 'cyan', zorder = 3)
+#Add title
+ax.flatten()[0].set_title('a) Digitised Points', fontsize = 16, fontweight = 'bold')
+
+#Display fits to these points on second image
+#Subscap
+#Fit line
+m,c = np.polyfit(ssPts_x, ssPts_y, 1)
+#Plot fitted line
+ax.flatten()[1].plot(ssPts_x, (m * ssPts_x + c), c = 'red', lw = 2, zorder = 5)
+#Glenoid plane
+#Fit line
+m,c = np.polyfit(gpPts_x, gpPts_y, 1)
+#Plot fitted line
+ax.flatten()[1].plot(gpPts_x, (m * gpPts_x + c), c = 'yellow', lw = 2, zorder = 4)
+#Humeral head
+#Fit circle
+hhCentreX, hhCentreY, hhRadius = fitCircle(hhPts_x, hhPts_y)
+#Plot cirlcle
+ax.flatten()[1].add_artist(plt.Circle((hhCentreX, hhCentreY), hhRadius,
+                                      edgecolor = 'cyan', facecolor = 'none', lw = 2,
+                                      zorder = 3))
+ax.flatten()[1].scatter(hhCentreX, hhCentreY, s = 20, c = 'cyan', zorder = 3)
+#Add title
+ax.flatten()[1].set_title('b) Geometrical Fits', fontsize = 16, fontweight = 'bold')
+
+#Display line of action calculation on third image
+#Subscap
+#Fit line
+subscapM, subscapC = np.polyfit(ssPts_x, ssPts_y, 1)
+#Plot fitted line
+ax.flatten()[2].plot(ssPts_x, (subscapM * ssPts_x + subscapC), c = 'red', lw = 2, zorder = 5)
+#Calculate intersection of subscap to glenoid plane
+glenoidM, glenoidC = np.polyfit(gpPts_x, gpPts_y, 1)
+#Extract two points on a line fit to the glenoid plane
+gx1 = 0
+gx2 = 100
+gy1 = glenoidM * gx1 + glenoidC #y-intercept; x = 0
+gy2 = glenoidM * gx2 + glenoidC #x = 100
+#Extract two points on the subscapularis linefit
+sx1 = 0
+sx2 = 100
+sy1 = subscapM * sx1 + subscapC #y-intercept; x = 0
+sy2 = subscapM * sx2 + subscapC #x = 100
+#Calculate line intersection
+intX, intY = lineIntersect(gx1, gy1, gx2, gy2, sx1, sy1, sx2, sy2)
+ax.flatten()[2].scatter(intX, intY, s = 20, c = 'yellow', zorder = 6)
+#Extend subscapularis line
+#Line
+ax.flatten()[2].plot(np.array(((ax.flatten()[2].get_xlim()[1] / 3), np.min(ssPts_x))),
+          subscapM * np.array(((ax.flatten()[2].get_xlim()[1] / 3), np.min(ssPts_x))) + subscapC,
+          c = 'red', lw = 2, ls = '--', zorder = 5)
+#Arrow head
+ax.flatten()[2].arrow(np.max(ssPts_x), subscapM * np.max(ssPts_x) + subscapC,
+                      (ax.flatten()[2].get_xlim()[1] / 3) - np.max(ssPts_x),
+                      (subscapM * (ax.flatten()[2].get_xlim()[1] / 3) + subscapC) - (subscapM * np.max(ssPts_x) + subscapC),
+                      head_width = 30, head_length = 30, 
+                      lw = 0, color = 'red', ls = ':', edgecolor = 'none',
+                      length_includes_head = False)
+#Plot the glenoid plane
+#Line
+ax.flatten()[2].plot(gpPts_x, (glenoidM * gpPts_x + glenoidC), c = 'yellow', lw = 2, zorder = 4)
+#Arrow head
+ax.flatten()[2].arrow(gpPts_x[0], glenoidM * gpPts_x[0] + glenoidC,
+                      gpPts_x[1] - gpPts_x[0],
+                      gpPts_x[1] - (glenoidM * gpPts_x[0] + glenoidC),
+                      head_width = 30, head_length = 30, 
+                      lw = 0, color = 'yellow', ls = ':', edgecolor = 'none',
+                      length_includes_head = True)
+#Extend the glenoid plane to the intersection point
+ax.flatten()[2].plot(np.array(((intX), np.min(gpPts_x))),
+          glenoidM * np.array(((intX), np.min(gpPts_x))) + glenoidC,
+          c = 'yellow', lw = 2, ls = '--', zorder = 4)
+#Plot the perpendicular line
+#Get gradient
+mPerp = 1 / (-glenoidM)
+#Work out y-intercept
+cPerp = ((mPerp*intX) - intY) / -1
+#Plot line
+ax.flatten()[2].plot(np.array(((intX), intX-250)),
+          mPerp * np.array(((intX), intX-250)) + cPerp,
+          c = 'lime', lw = 2, zorder = 3)
+#Arrow head
+ax.flatten()[2].arrow(intX, intY,
+                      -250, 
+                      (mPerp * (intX-250) + cPerp) - intY,
+                      head_width = 30, head_length = 30, 
+                      lw = 0, color = 'lime', ls = ':', edgecolor = 'none',
+                      length_includes_head = False)
+#Add title
+ax.flatten()[2].set_title('c) Line of Action Calculation', fontsize = 16, fontweight = 'bold')
+
+#Display moment arm calculation on fourth image
+#Subscap
+#Fit line
+subscapM, subscapC = np.polyfit(ssPts_x, ssPts_y, 1)
+#Plot fitted line
+ax.flatten()[3].plot(ssPts_x, (subscapM * ssPts_x + subscapC), c = 'red', lw = 2, zorder = 5)
+#Extend subscap line
+ax.flatten()[3].plot(np.array(((ax.flatten()[2].get_xlim()[0] + (np.max(ssPts_x)/4)), np.max(ssPts_x))),
+          subscapM * np.array(((ax.flatten()[2].get_xlim()[0] + (np.max(ssPts_x)/4)), np.max(ssPts_x))) + subscapC,
+          c = 'red', lw = 2, ls = '--', zorder = 5)
+#Plot humeral head centre
+ax.flatten()[3].scatter(hhCentreX, hhCentreY, s = 20, c = 'cyan', zorder = 4)
+#Calculate the point on the extended subscapularis line that is intercepted
+#by a perpendicular line from the humerus
+#Set the points at the start and the end of the subscapularis line
+x1 = ax.flatten()[2].get_xlim()[0] + (np.max(ssPts_x)/4)
+y1 = subscapM * x1 + subscapC
+x2 = np.max(ssPts_x)
+y2 = subscapM * x2 + subscapC
+#Set the humeral head point
+x3 = hhCentreX
+y3 = hhCentreY
+#Calculate intersecting point with perpendicular line
+#SEE: https://stackoverflow.com/questions/1811549/perpendicular-on-a-line-from-a-given-point
+k = ((y2-y1) * (x3-x1) - (x2-x1) * (y3-y1)) / ((y2-y1)**2 + (x2-x1)**2)
+x4 = x3 - k * (y2-y1)
+y4 = y3 + k * (x2-x1)
+#Plot the perpendicular line between the points
+#This is effectively the moment arm - but only use here as visualisation
+ax.flatten()[3].plot(np.array((x3,x4)),np.array((y3,y4)),
+                     c = 'cyan', lw = 2, zorder = 3)
+#Add title
+ax.flatten()[3].set_title('d) Moment Arm Calculation', fontsize = 16, fontweight = 'bold')
+
+#Save figure
+fig.savefig('..\\Results\\Figures\\methods_Fig.png', dpi = 300, format = 'png')
 
 #Close figure
 plt.close()
